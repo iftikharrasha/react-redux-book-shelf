04f54dd1211dc37e5eeb6781e636caad39ee17e2
intisar eusha <iftikharrasha@gmail.com>
2021-12-14 19:40:55 +0600

    first commit

---
+++ src/redux/actions/bookActions.js
@@ -0,0 +1,13 @@
+export const addToReadingList = (payload) => {
+    return {
+        type: 'ADD_TO_READING_LIST',
+        payload
+    }
+}
+
+export const removeFromReadingList = (payload) => {
+    return {
+        type: 'REMOVE_FROM_READING_LIST',
+        payload
+    }
+}
